06d74b2b5e59a08c7f904355369f25b65a2fa940
Fauzy <haliffauzy45@gmail.com>
2024-03-14 15:57:35 +0700

    change code

--- src/components/Anime/BoxComment.jsx
+++ src/components/Anime/BoxComment.jsx
@@ -6,7 +6,7 @@ const CommentBox = async ({ anime_mal_id }) => {
     const comments = await prisma.comment.findMany({ where: { anime_mal_id } });
 
     return (
-        <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4 mb-4">
+        <div className="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-4 mb-4">
             {comments.map(comment => (
                 <div key={comment.id} className="flex items-center text-color-dark bg-color-primer p-4 rounded border-2 border-color-dark">
                     <div className="mr-2">
@@ -15,7 +15,7 @@ const CommentBox = async ({ anime_mal_id }) => {
                     </div>
                     <div>
                         <p className="font-bold">@{comment.username}</p>
-                        <p className="text-sm">{comment.comment}</p>
+                        <p>{comment.comment}</p>
                     </div>
                 </div>
             ))}
