89b5c55e3ae1f08755adc14b2d4695ead32bdb32
Fauzy <haliffauzy45@gmail.com>
2024-03-15 01:20:59 +0700

    change code

--- next.config.js
+++ next.config.js
@@ -16,7 +16,7 @@ const nextConfig = {
             },
             
             {
-                hostname: "scontent.fbdo9-1.fna.fbcdn.net"
+                hostname: "platform-lookaside.fbsbx.com"
             }
         ]
     }
