a2c6fb80733d22b742705dec824fb67daa348921
fauzyDev <haliffauzy45@gmail.com>
2026-04-24 19:39:46 +0700

    update: bun

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     images: {
         remotePatterns: [
             {
-                hostname: "cdn.myanimelist.net"
+                hostname: "myanimelist.net"
             },
 
             {
